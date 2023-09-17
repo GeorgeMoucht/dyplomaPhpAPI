3136915046fcb86f2325e4c62f133a2b66c87622
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-09-18 02:44:50 +0300

    Return to just JWT auth system

--- routes/api.php
+++ routes/api.php
@@ -24,29 +24,3 @@ Route::middleware('api')->group(function () {
     Route::post('login', [AuthController::class, 'login'])->name('login');
     Route::post('logout', [AuthController::class, 'logout']);
 });
-
-Route::middleware(['api', 'jwt'])->group(function () {
-    // Allow authenticated user to create a new order
-    Route::post('/orders', 'OrderController@create');
-
-    // List a Customer's Order
-    Route::get('/customers/{customer_id}/orders', 'OrderController@index');
-
-    // View Order Details
-    Route::get('/orders/{order_id}', 'OrderController@show');
-
-    // Update Order Status
-    Route::put('/orders/{order_id}', 'OrderController@update');
-
-    // Delete an Order
-    Route::delete('/orders/{order_id}', 'OrderController@destroy');
-
-    // Add Product to Order
-    Route::post('/orders/{order_id}/products', 'OrderDetailController@store');
-
-    // Remove Product from Order
-    Route::delete('/orders/{order_id}/products/{order_details_id', 'OrderDetailController@destroy');
-
-    // List Products
-    Route::get('/products', 'ProductController@index');
-});
