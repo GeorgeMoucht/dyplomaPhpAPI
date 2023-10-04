2a845eb930118a916429194cc893d1fe1cffc47f
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-10-04 18:06:34 +0300

    Add id in the table

--- database/migrations/2023_10_03_104239_create_user_has_group_table.php
+++ database/migrations/2023_10_03_104239_create_user_has_group_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_has_group', function (Blueprint $table) {
+            $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('group_id');
 
